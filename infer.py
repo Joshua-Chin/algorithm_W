7d4782bb125c9b97daac75c8a31c48b37fc5ce01
Joshua Chin <joshuarchin@gmail.com>
2018-04-14 21:13:02 -0400

    major rewrite

--- infer.py
+++ infer.py
@@ -1,125 +1,158 @@
-from functools import namedtuple
+from multipledispatch import dispatch
 
-from union_find import UnionFind
-from data import *
-
+import exprs
+import types
+from disjointset import DisjointSet
 
 class Environment(object):
 
     def __init__(self):
-        self._types = UnionFind()
+        self._types = DisjointSet()
         self._typings = {}
-        self._typevar_index = 0
+        self._typevars = 0
 
-    def infer_type(self, expr):
-        """
-        Infers the type of an expression.
-        """
-        return self.generalize(self._infer_type(expr))
 
-    def _infer_type(self, expr):
-        # literals have known type
-        if isinstance(expr, literal):
-            return type_literal(type(expr.value))
-        # variables have a type depending on the environment
-        elif isinstance(expr, variable):
-            return self.specialize(self._types.find(self._typings[expr]))
-        # function calls require unifying the argument and the function
-        elif isinstance(expr, call):
-            functype = self._infer_type(expr.function)
-            argtype = self._infer_type(expr.arg)
-            result_type = self.newvar()
-            self.unify(functype, function_type(argtype, result_type))
-            return self._types.find(result_type)
-        # function definition
-        elif isinstance(expr, function):
-            argtype = self.newvar()
-            self._typings[expr.arg] = argtype
-            body_type = self._infer_type(expr.body)
-            return function_type(self._types.find(argtype), body_type)
-        elif isinstance(expr, let):
-            vartype = self.newvar()
-            self._typings[expr.var] = vartype
-            valuetype = self._infer_type(expr.value)
-            self.unify(vartype, valuetype)
-            self._typings[expr.var] = self.generalize(self._types.find(vartype))
-            return self._infer_type(expr.body)
-        raise TypeError(f"Unrecognized expression type: {type(expr)}")
+    def typeof(self, expr):
+        """Returns the generalized type of an expression"""
+        return self.generalize(self.w(expr))
+
+    @dispatch(exprs.literal)
+    def w(self, literal):
+        """Returns the type of an expression"""
+        # literals have a fixed type
+        return types.literal(type(literal.value))
+
+    @dispatch(exprs.variable)
+    def w(self, variable):
+        # variables have a type that depending on the environment
+        return self.specialize(self.find(self._typings[variable]))
+
+    @dispatch(exprs.call)
+    def w(self, call):
+        function = self.w(call.function)
+        arg = self.w(call.arg)
+        result = self.newvar()
+        self.unify(func, types.function(arg, result))
+        return self.find(result)
+
+    @dispatch(exprs.function)
+    def w(self, function):
+        arg = self.newvar()
+        self._typings[function.arg] = arg
+        body = self.w(expr.body)
+        return types.function(self.find(arg), body)
+
+    @dispatch(exprs.let)
+    def w(self, function):
+        # insert expr.var into typings to allow recursion
+        var = self.newvar()
+        self._typings[expr.var] = var
+        # compute type of value and unify it with var
+        value = self.w(expr.value)
+        self.unify(var, value)
+        # generalize the type of var
+        self._typings[expr.var] = self.generalize(value)
+        return self.w(expr.body)
+
 
     def newvar(self):
-        """
-        Returns a new type variable unique within the environment.
-        """
-        self._typevar_index += 1
-        return type_variable(self._typevar_index)
+        """Returns a new type variable unique within the environment"""
+        self._typevars += 1
+        return types.variable(self._typevars)
 
-    def specialize(self, type_, substitions=None):
-        """
-        Specializes the type into a monomorphic form by substituting bound variables.
-        """
-        if substitions is None:
-            substitions = {}
+
+    @dispatch(types.literal)
+    def find(self, literal):
+        """Finds a representation of the type"""
+        return literal
+
+    @dispatch(types.variable)
+    def find(self, variable):
+        type = self._types.find(variable)
+        return type if type == variable else self.find(type)
+
+    @dispatch(types.function)
+    def find(self, function):
+        return types.function(
+            self.find(function.arg),
+            self.find(function.result))
+
+
+    @dispatch(object)
+    def specialize(self, type):
+        """Specializes a polymorphic type into a monomorphic one"""
+        return self.specialize(type, {})
+
+    @dispatch((types.literal, object))
+    def specialize(self, literal, subs):
         # type literals are already monomorphic
-        if isinstance(type_, type_literal):
-            return type_
-        # type variables may be polymorphic within a context
-        elif isinstance(type_, type_variable):
-            return substitions.get(type_, type_)
-        # function_types may have polymorphic bodies
-        elif isinstance(type_, function_type):
-            arg = self.specialize(self._types.find(type_.arg), substitions)
-            result = self.specialize(self._types.find(type_.result), substitions)
-            return function_type(arg, result)
-        # polymorphic forms are clearly polymorphic
-        elif isinstance(type_, polymorphic_type):
-            substitions.update({
-                self._types.find(bound_type): self.newvar()
-                for bound_type in type_.bound_types
-            })
-            return self.specialize(self._types.find(type_.type), substitions)
-        else:
-            raise TypeError(f"Unrecognized metatype: {type(type_)}")
+        return literal
 
-    def unify(self, x, y, seen=None):
-        """
-        Unifies two types within the environment.
-        """
-        x = self._types.find(x)
-        y = self._types.find(y)
+    @dispatch((types.variable, object))
+    def specialize(self, variable, subs):        
+        # type variables may be polymorphic within a context
+        return substitions.get(variable, variable)
+        
+        # function_types may have polymorphic bodies
+    @dispatch((types.function, object))
+    def specialize(self, function, subs):
+            arg = self.specialize(function.arg, subs)
+            result = self.specialize(function.result, subs)
+            return types.function(arg, result)
+
+    @dispatch((types.polymorphic, object))
+    def specialize(self, polymorphic, subs):
+        # polymorphic forms are clearly polymorphic
+        subs.update({
+            bound_type: self.newvar()
+            for bound_type in polymorphic.bound_types
+        })
+        return self.specialize(polymorphic.type, substitions)
+
+
+    def unify(self, x, y):
+        """Unifies two types"""
+        x = self.find(x)
+        y = self.find(y)
         # if they are equal, they are already unified.
         if x == y:
-            return
-        # if both are functions, try to unify their arguments and results
-        elif isinstance(x, function_type) and isinstance(y, function_type):
+            pass
+        # if both are functions, they can be unified
+        elif isinstance(x, types.function) and \
+           isinstance(y, types.function):
             self.unify(x.arg, y.arg)
             self.unify(x.result, y.result)
+            return
         # if either is a type variable, they can be unified
-        elif isinstance(x, type_variable) or isinstance(y, type_variable):
-            if x in self.free_types(y) or y in self.free_types(x):
-                raise ValueError("Recursive type detected")
-            self._types.unify(x, y)
-        else:
-            raise TypeError(f'type {x} and type {y} cannot be unified')
+        elif isinstance(x, types.variable) or
+             isinstance(y, types.variable):
+            # ensure that `y` is a type variable
+            if isinstance(x, types.variable):
+                x, y = y, x
+            # check for infinite types
+            if y in self.free_types(x):
+                raise TypeError(f'unifying type {x} and type {y} will result in infinite type')
+            self._types.merge(x, y)
+            return
 
-    def generalize(self, type_):
-        """
-        Generalizes a monomorphic type with free variables into a polymorphic form.
-        """
-        free_types = self.free_types(type_)
-        if free_types:
-            return polymorphic_type(self.free_types(type_), type_)
-        else:
-            return type_
+        raise TypeError(f'type {x} and type {y} cannot be unified')
 
-    def free_types(self, type_):
-        if isinstance(type_, type_literal):
-            return frozenset()
-        elif isinstance(type_, type_variable):
-            return frozenset({self._types.find(type_)})
-        elif isinstance(type_, function_type):
-            return self.free_types(self._types.find(type_.arg)) | \
-                self.free_types(self._types.find(type_.result))
-        elif isinstance(type_, polymorphic_type):
-            return self.free_types(self._types.find(type_.type)) - \
-                {self._types.find(bound_type) for bound_type in type_.bound_types}
-        raise TypeError(f"Unrecognized metatype: {type(type_)}")
+
+    def generalize(self, type):
+        """Attempts to generalize a monomorphic type into a polymorphic one"""
+        free_types = self.free_types(type)
+        if not free_types: return type
+        return types.polymorphic(free_types, type)
+
+    @dispatch(types.literal)
+    def free_types(self, literal):
+        return frozenset()
+
+    @dispatch(types.variable)
+    def free_types(self, variable):
+            return frozenset({variable})
+
+    @dispatch(types.function)
+    def free_types(self, function):
+            return self.free_types(function.arg) | \
+                   self.free_types(function.result)
